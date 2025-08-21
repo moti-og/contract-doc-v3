65ef3cbeec61cab3a028be0113bd929d2b448696
Moti <msorkin@opengov.com>
2025-08-21 14:32:58 -0700

    ci+tests: add Jest API tests and GitHub Actions workflow; CI smoke

---
+++ server/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  verbose: false,
+  forceExit: true,
+  testTimeout: 30000,
+};
+
+
